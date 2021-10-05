3818495de7474a53e3d6acaade2af411956900b8
Thom <hsbang.thom@gmail.com>
2021-10-05 18:28:13 +0900

    settings for deployment automation

---
+++ scripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=toy-springboot
+
+echo "> copying build files.."
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> checking current application PID.."
+
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}' )
+
+echo "> current application PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID"]; then
+  echo "> no application is running "
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> deployment on new application is processing.."
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR name : $JAR_NAME"
+
+echo "> grant execution permission to $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME is running"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
