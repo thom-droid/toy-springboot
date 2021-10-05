23194d95ef92d119be88a8ab1fcd93f15e9a7958
Thom <hsbang.thom@gmail.com>
2021-10-05 23:09:40 +0900

    deployment BeforeInstall script -- 2

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d /home/ec2-user/app/step2/zip ]; then
-  sudo rm -rf /home/ec2-user/app/step2/zip
+if [ -d /home/ec2-user/app/step2 ]; then
+  sudo rm -rf /home/ec2-user/app/step2/
 fi
 sudo mkdir -vp /home/ec2-user/app/step2/zip
